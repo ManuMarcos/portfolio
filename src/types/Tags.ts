c3080558d68351c878a5d71cab1600f04f681709
ManuMarcos <manuelignaciomarcos@hotmail.com>
2025-02-28 00:47:32 -0300

    feat: small cards to little projects

--- src/types/Tags.ts
+++ src/types/Tags.ts
@@ -1,6 +1,10 @@
 
 import type { Tags } from "../components/ProjectCard.astro";
+import Css from "../icons/Css.astro";
+import DesignPattern from "../icons/DesignPattern.astro";
+import Html from "../icons/Html.astro";
 import Java from "../icons/Java.astro";
+import Javascript from "../icons/Javascript.astro";
 import Mongo from "../icons/Mongo.astro";
 import MySQL from "../icons/MySQL.astro";
 import Next from "../icons/Next.astro";
@@ -37,6 +41,26 @@ export const TAGS = {
     name: "MongoDB",
     class: "bg-[#081d2a] text-green font-semibold border border-green-800 shadow-md",
     icon: Mongo
+  },
+  DESSIGN_PATTERN:{
+    name: "Dessign Patterns",
+    class: "bg-black text-white font-semibold border border-gray-600 shadow-md",
+    icon: DesignPattern
+  },
+  JAVASCRIPT:{
+    name: "Javascript",
+    class: "bg-black text-white font-semibold border border-gray-600 shadow-md",
+    icon: Javascript
+  },
+  HTML:{
+    name: "HTML",
+    class: "bg-black text-white font-semibold border border-gray-600 shadow-md",
+    icon: Html
+  },
+  CSS:{
+    name: "CSS",
+    class: "bg-black text-white font-semibold border border-gray-600 shadow-md",
+    icon: Css
   }
 } as const;
 
